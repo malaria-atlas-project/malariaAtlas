1243367b58c78ee291ed6e2d3f9f3587270ff1ec
Dan Pfeffer <daniel.pfeffer@well.ox.ac.uk>
2017-05-15 13:14:40 +0100

    Changed geoserver table that is being queried to species-combined table.
    Added an if statement that checks whether specified country is in the list of available countries and if not returns names of available countries.

--- R/getPR.R
+++ R/getPR.R
@@ -19,60 +19,49 @@
 #' getPR(country = "ALL", species = "Pv")
 #' @export
 getPR <- function(country, species) {
+
+  URL <- "http://map-prod3.ndph.ox.ac.uk/geoserver/Explorer/ows?service=wfs&version=2.0.0&request=GetFeature&outputFormat=csv&TypeName=surveys_pr"
+  available_countries <- c(levels(utils::read.csv(paste(URL, "&PROPERTYNAME=country", sep = ""))$country))
+
+  if(!(country %in% available_countries))
+  {stop("No data available for ",country," use one of: \n",
+              paste(available_countries, collapse = "\n "))}
+
   if(species == "BOTH") {
+
+    columns <- "&PROPERTYNAME=id,month_start,year_start,month_end,year_end,lower_age,upper_age,examined,pf_pos,pf_positive,pf_pr,method,rdt_type,pcr_type,latitude,longitude,name,area_type_id,rural_urban,country_id,country,continent_id,who_region_id,citation1,site_id"
 
     ### SET UP AND TEST A SECTION TO COMBINE/MERGE BOTH PF&PV
 
   } else if(species == "Pf") {
 
-    URL <- "http://map-prod3.ndph.ox.ac.uk/geoserver/Explorer/ows?service=wfs&version=2.0.0&request=GetFeature&outputFormat=csv&TypeName=surveys_pfpr"
     columns <- "&PROPERTYNAME=id,month_start,year_start,month_end,year_end,lower_age,upper_age,examined,pf_pos,pf_positive,pf_pr,method,rdt_type,pcr_type,latitude,longitude,name,area_type_id,rural_urban,country_id,country,continent_id,who_region_id,citation1,site_id"
 
-    if("ALL" %in% country){
-      message("Importing PfPR point data for all locations, please wait...")
-      df <-   utils::read.csv(paste(URL,columns,"&cql_filter=is_available=%27true%27",sep = ""))[,-1]
-      return(df)
-
-    }else{
-      message(paste("Importing PfPR point data for", paste(country, collapse = ", "), "please wait..."))
-      country.list <- paste("%27",country, "%27", sep = "", collapse = "," )
-      df <- utils::read.csv(paste(URL,
-                                  columns,
-                                  "&cql_filter=country%20IN%20(",
-                                  country.list,
-                                  ")%20AND%20is_available=%27true%27", sep = ""))[,-1]
-      if(length(df$id) == 0) {
-        stop("No data downloaded: - check spelling of country name and/or availability of data for specificed country.")
-      } else {
-        return(df)
-      }
-    }
   } else if(species == "Pv") {
 
-    URL <- "http://map-prod3.ndph.ox.ac.uk/geoserver/Explorer/ows?service=wfs&version=2.0.0&request=GetFeature&outputFormat=csv&TypeName=surveys_pvpr"
     columns <- "&PROPERTYNAME=id,month_start,year_start,month_end,year_end,lower_age,upper_age,examined,pv_pos,pv_positive,pv_pr,method,rdt_type,pcr_type,latitude,longitude,name,area_type_id,rural_urban,country_id,country,continent_id,who_region_id,citation1,site_id"
 
-    if("ALL" %in% country){
-      message("Importing PvPR point data for all locations, please wait...")
-      df <-   utils::read.csv(paste(URL,columns,"&cql_filter=is_available=%27true%27",sep = ""))[,-1]
+  } else {stop("Species not recognized, use one of: \n   \"Pf\" \n   \"Pv\" \n   \"BOTH\"")}
+
+  if("ALL" %in% country){
+    message(paste("Importing",species,"PR point data for all locations, please wait...", sep = ""))
+    df <-   utils::read.csv(paste(URL,columns,"&cql_filter=is_available=%27true%27",sep = ""))[,-1]
+    return(df)
+
+  }else{
+    message(paste("Importing PfPR point data for", paste(country, collapse = ", "), "please wait..."))
+    country.list <- paste("%27",country, "%27", sep = "", collapse = "," )
+    df <- utils::read.csv(paste(URL,
+                                columns,
+                                "&cql_filter=country%20IN%20(",
+                                country.list,
+                                ")%20AND%20is_available=%27true%27", sep = ""))[,-1]
+    if(length(df$id) == 0) {
+      stop("No data downloaded: - check spelling of country name and/or availability of data for specificed country.")
+    } else {
       return(df)
-
-    }else{
-      message(paste("Importing PvPR point data for", paste(country, collapse = ", "), "please wait..."))
-      country.list <- paste("%27",country, "%27", sep = "", collapse = "," )
-      df <- utils::read.csv(paste(URL,
-                                  columns,
-                                  "&cql_filter=country%20IN%20(",
-                                  country.list,
-                                  ")%20AND%20is_available=%27true%27", sep = ""))[,-1]
-      if(length(df$id) == 0) {
-        stop("No data downloaded - check spelling of country name and/or availability of data for specificed country.")
-      } else {
-        return(df)
-      }
     }
-
-  } else {stop("Provided species argument not recognized, use one of: \n   \"Pf\" \n   \"Pv\" \n   \"BOTH\"")}
+  }
 
 }
 
@@ -80,24 +69,20 @@ getPR <- function(country, species) {
 # pr_data <- getPR(country = c("Nigeria", "Madagascar"), species = "Pf")
 
 # all possible columns:
-# names(PfPR_points_ALL)
 #
-# names(PfPR_points_ALL)
-# > [1] "FID"                 "id"                  "month_start"         "year_start"          "month_end"
-# > [6] "year_end"            "lower_age"           "upper_age"           "examined"            "pf_pos"
-# > [11] "pf_pr"               "pf_positive"         "dhs_id"              "admin1_paper"        "admin2_paper"
-# > [16] "admin3_paper"        "method"              "rdt_type"            "survey_notes"        "pcr_type"
-# > [21] "site_id"             "admin_id"            "latitude"            "longitude"           "name"
-# > [26] "latlong_source_id"   "site_notes"          "forest"              "rice"                "country_id"
-# > [31] "area_type_id"        "rural_urban"         "geom"                "country_code"        "country"
-# > [36] "pf_endemic"          "pv_endemic"          "continent_id"        "area"                "eliminating"
-# > [41] "geo_region_id"       "map_region_id"       "who_region_id"       "source_id1"          "year1"
-# > [46] "pdf_status_id1"      "title1"              "citation1"           "source_type_id1"     "contact_id1"
-# > [51] "permission_type_id1" "cinfidential2"       "pdf_status_id2"      "title2"              "citation2"
-# > [56] "source_id2"          "source_type_id2"     "contact_id2"         "year2"               "permission_type_id2"
-# > [61] "confidential3"       "pdf_status_id3"      "title3"              "citation3"           "source_type_id3"
-# > [66] "contact_id3"         "source_id3"          "year3"               "permission_type_id3" "time_start"
-# > [71] "time_end"            "is_available"
+# names(PR_points_ALL)
+# [1] "FID"                 "id"                  "month_start"         "year_start"          "month_end"           "year_end"
+# [7] "lower_age"           "upper_age"           "examined"            "pf_pos"              "pv_pos"              "pf_pr"
+# [13] "pv_pr"               "pf_positive"         "pv_positive"         "dhs_id"              "admin1_paper"        "admin2_paper"
+# [19] "admin3_paper"        "method"              "rdt_type"            "survey_notes"        "pcr_type"            "site_id"
+# [25] "admin_id"            "latitude"            "longitude"           "name"                "latlong_source_id"   "site_notes"
+# [31] "forest"              "rice"                "country_id"          "area_type_id"        "rural_urban"         "country"
+# [37] "pf_endemic"          "pv_endemic"          "continent_id"        "area"                "eliminating"         "geo_region_id"
+# [43] "map_region_id"       "who_region_id"       "source_id1"          "year1"               "pdf_status_id1"      "title1"
+# [49] "citation1"           "source_type_id1"     "contact_id1"         "permission_type_id1" "cinfidential2"       "pdf_status_id2"
+# [55] "title2"              "citation2"           "source_id2"          "source_type_id2"     "contact_id2"         "year2"
+# [61] "permission_type_id2" "pdf_status_id3"      "title3"              "citation3"           "source_type_id3"     "contact_id3"
+# [67] "source_id3"          "year3"               "permission_type_id3"
 
 
 
