968866655fd64c6719f005227204a0c43ce3ae86
Dan Pfeffer <daniel.pfeffer@well.ox.ac.uk>
2017-05-23 11:58:17 +0100

    fixed data that is both printed and returned

--- R/listAll.R
+++ R/listAll.R
@@ -10,14 +10,12 @@
 listAll <- function() {
   message("Creating list of countries for which PR data is available, please wait...")
   x <- utils::read.csv("http://map-prod3.ndph.ox.ac.uk/geoserver/Explorer/ows?service=wfs&version=2.0.0&request=GetFeature&outputFormat=csv&TypeName=surveys_pr&PROPERTYNAME=country", encoding = "UTF-8")
-  print("Countries with PR Data:")
-  cat(paste(levels(x$country), collapse = " \n "))
-  return( )
+  message("Countries with PR Data: \n ",paste(levels(x$country), collapse = " \n "))
+  return(invisible(levels(x$country)))
   }
 
 
 # listAll()
-
 
 
 
