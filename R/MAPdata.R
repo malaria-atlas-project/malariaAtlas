72f5f0e3fe6f591ba14e33d703712bfb51db0398
Dan Pfeffer <daniel.pfeffer@well.ox.ac.uk>
2017-06-15 17:25:01 +0100

    Updated and filled out documentation for various functions. #7

--- R/MAPdata.R
+++ R/MAPdata.R
@@ -1,20 +1,17 @@
-#' Download publicly available Parasite Rate Data from the Malaria Atlas Project geoserver.
+#' Download publicly available Parasite Rate Survey Points from the Malaria Atlas Project database.
 #'
 #' \code{MAPdata} provides a suite of tools to allow you to
-
 #'  download all publicly available PR points for a specified country
-
-
-#'  (or ALL countries) into R as a dataframe.
+#'  (or ALL countries) as a dataframe within R.
 #'
 #' @section MAPdata functions:
-#'  \enumerate {
-#'  \item listAll - what does it do?
-#'  \item getPR - what does it do?
+#'  \enumerate{
+#'  \item \code{listAll} - lists all countries for which there are publicly visible PR datapoints in the MAP database.
+#'  \item \code{is_available} - checks whether the MAP database contains PR points for the specified country/countries.
+#'  \item \code{getPR} - downloads all publicly available PR points for a specified country (or countries) and returns this as a dataframe.
 #'  }
 #' @docType package
 #' @name MAPdata
 NULL
 
-# devtools::use_package(package = "utils")
 
