e290511187f8ba6ba53813d7d92350adb11a9269
Dan Pfeffer <daniel.pfeffer@well.ox.ac.uk>
2017-06-15 09:05:58 +0100

    added ISOs to listAll() (and tidied it a bit).

--- R/listAll.R
+++ R/listAll.R
@@ -9,12 +9,13 @@
 #' @export
 listAll <- function(printed = TRUE) {
   message("Creating list of countries for which PR data is available, please wait...")
-  x <- utils::read.csv("http://map-prod3.ndph.ox.ac.uk/geoserver/Explorer/ows?service=wfs&version=2.0.0&request=GetFeature&outputFormat=csv&TypeName=surveys_pr&PROPERTYNAME=country", encoding = "UTF-8")
+  x <- unique(read.csv("http://map-prod3.ndph.ox.ac.uk/geoserver/Explorer/ows?service=wfs&version=2.0.0&request=GetFeature&outputFormat=csv&TypeName=surveys_pr&PROPERTYNAME=country,country_id", encoding = "UTF-8")[,c("country", "country_id")])
+  x$country_and_iso <- paste(x$country," (",x$country_id, ")", sep = "")
   if(printed == TRUE){
-    message("Countries with PR Data: \n ",paste(levels(x$country), collapse = " \n "))
+    message("Countries with PR Data: \n ",paste(x$country_and_iso, collapse = " \n "))
   }
 
-  return(invisible(levels(x$country)))
+  return(invisible(x))
   }
 
 
