8f1b059ffad9c9d506b3a0d3d111619459b813ef
Dan Pfeffer <daniel.pfeffer@well.ox.ac.uk>
2017-08-02 17:18:06 +0100

    testing my config

--- tests/.tests_for_CI.R
+++ tests/.tests_for_CI.R
@@ -1,11 +1,5 @@
 ## Run devtools check and see whether or not it returns any errors
 
-install.packages("devtools", repos = "https://cran.ma.imperial.ac.uk/")
-
-library(devtools)
-
-devtools::install_deps()
-
 library(ggplot2)
 
 test_results <- devtools::check()
