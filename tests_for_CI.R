c9b74f7b50d7338d93001523ec59006651e075bf
Dan Pfeffer <daniel.pfeffer@well.ox.ac.uk>
2017-08-02 10:15:01 +0100

    adding tests_for_CI.R

---
+++ tests_for_CI.R
@@ -0,0 +1,8 @@
+## Run devtools check and see whether or not it returns any errors
+
+test_results <- devtools::check()
+
+if(length(test_results$errors)>0){
+  stop(paste("Pipeline failed due to the following errors:\n",paste(test_results$errors, collapse = "\n"), sep = ""))
+}
+
