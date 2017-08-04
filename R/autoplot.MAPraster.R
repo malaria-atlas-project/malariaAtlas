ba56ba471ccc3bfd1ab2c48749b569d66f7a1c7a
Dan Pfeffer <daniel.pfeffer@well.ox.ac.uk>
2017-08-04 15:04:41 +0100

    adding initial version of getRaster & associated autoplot

---
+++ R/autoplot.MAPraster.R
@@ -0,0 +1,25 @@
+autoplot.MAPraster <- function(object, country = NULL, shp_df = NULL, legend = ""){
+
+  plot <- ggplot()+
+    geom_tile(data = object, aes(x=x, y=y, fill = object[,1]))+
+    coord_equal()+
+    scale_fill_distiller(name = paste(legend), palette = "RdYlBu")+
+  theme(plot.title = element_text(vjust=-1),
+        panel.background = element_rect(fill = "white"),
+        panel.grid = element_blank(),
+        axis.title = element_blank(),
+        panel.border = element_rect(colour = "grey50", fill=NA, size = 0.5))
+
+  if(!is.null(shp_df)){
+    plot$layers <- c(geom_polygon(data = shp_df, aes(x=long, y=lat, group = group), fill = "grey65"),
+                     plot$layers)
+    plot <- plot + geom_polygon(data = shp_df, aes(x=long, y=lat, group = group), alpha = 0, colour = "black")
+  } else if(!is.null(country)){
+    plot_shp <- getShp(country = country, admin_level = "admin0", format = "df")[[1]]
+    plot$layers <- c(geom_polygon(data = plot_shp, aes(x=long, y=lat, group = group), fill = "grey65"),
+                     plot$layers)
+    plot <- plot + geom_polygon(data = plot_shp, aes(x=long, y=lat, group = group), alpha = 0, colour = "black")
+}
+  return(plot)
+}
+
--- R/getRaster.R
+++ R/getRaster.R
@@ -1,25 +1,72 @@
-# getRaster <- function(object, bbox = NULL){
-#
-# if(is.null(bbox)){
-#
-# bbox_0.02 <- function(x){
-#     bbox <-  c(min(x$latitude[!is.na(x$latitude)]),
-#                min(x$longitude[!is.na(x$longitude)]),
-#                max(x$latitude[!is.na(x$latitude)]),
-#                max(x$longitude[!is.na(x$longitude)]))
-#
-#     bbox[1:2] <- bbox[1:2]-(0.02*abs(bbox[1:2]))
-#     bbox[3:4] <- bbox[3:4]+(0.02*abs(bbox[3:4]))
-#
-#     return(bbox)
-#   }
-#
-# bbox <- bbox_0.02(object)
-# }
-#
-#
-# }
-#
-#
-#
-# x <- raster::raster("http://map-prod3.ndph.ox.ac.uk/geoserver/ows?service=WCS&version=2.0.1&request=GetCoverage&format=geotiff&coverageid=africa-pr-2000-2015&BBOX(the_geom,-26.06751,42.68112,-11.89176,51.27320)")
+getRaster <- function(surface = "PfPR_2010", shp = NULL, bbox = NULL, file_path = tempdir(), format = "rasterlayer"){
+
+  if(!is.null(bbox)){
+    bbox <- bbox
+  }else if (!is.null(shp)){
+  bbox <- sp::bbox(shp)
+  }
+
+  if(surface == "PfPR_2010"){
+    raster_code <- "ST_PR_mean"
+  }else if(surface == "funestus"){
+    raster_code <- "Anophele funestus"
+  }
+
+  xml_rqst <- paste("<?xml version=\"1.0\" encoding=\"UTF-8\"?><GetCoverage version=\"1.0.0\" service=\"WCS\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://www.opengis.net/wcs\" xmlns:ows=\"http://www.opengis.net/ows/1.1\" xmlns:gml=\"http://www.opengis.net/gml\" xmlns:ogc=\"http://www.opengis.net/ogc\" xsi:schemaLocation=\"http://www.opengis.net/wcs http://schemas.opengis.net/wcs/1.0.0/getCoverage.xsd\">
+  <sourceCoverage>Explorer:",paste(raster_code),"</sourceCoverage>
+  <domainSubset>
+    <spatialSubset>
+      <gml:Envelope srsName=\"EPSG:4326\">
+        <gml:pos>",paste(bbox[,1], collapse = " "),"</gml:pos>
+        <gml:pos>",paste(bbox[,2], collapse = " "),"</gml:pos>
+      </gml:Envelope>
+      <gml:Grid dimension=\"2\">
+        <gml:limits>
+          <gml:GridEnvelope>
+            <gml:low>0 0</gml:low>
+            <gml:high>288 230</gml:high>
+          </gml:GridEnvelope>
+        </gml:limits>
+        <gml:axisName>x</gml:axisName>
+        <gml:axisName>y</gml:axisName>
+      </gml:Grid>
+    </spatialSubset>
+  </domainSubset>
+  <output>
+    <crs>EPSG:4326</crs>
+    <format>GeoTIFF</format>
+  </output>
+</GetCoverage>", sep = "")
+
+
+  hdl <- curl::new_handle()
+  curl::handle_setopt(hdl,
+                copypostfields = paste(xml_rqst))
+  curl::handle_setheaders(hdl,
+                    "Content-Type" = "application/xml",
+                    "Cache-Control" = "no-cache")
+
+  rstdir <- file.path(file_path,"getRaster")
+  dir.create(rstdir)
+  temp_rst <- tempfile(pattern = paste(surface,"_",sep = ""), tmpdir = rstdir, fileext = ".tiff")
+
+  curl::curl_download(url = "http://map-prod3.ndph.ox.ac.uk/geoserver/wps", destfile = temp_rst, handle = hdl)
+
+  rst_dl <- raster::raster(temp_rst)
+
+  if(!is.null(shp)){
+    rst_dl <- raster::mask(rst_dl, shp)
+  }
+
+  if(tolower(format) == "rasterlayer"){
+    return(rst_dl)
+  } else if(tolower(format) == "df"){
+    rst_spdf <- as(rst_dl, "SpatialPixelsDataFrame")
+    rst_df <- as.data.frame(rst_spdf)
+    class(rst_df) <- c(class(rst_df), "MAPraster")
+    return(rst_df)
+  }
+}
+
+## TEST <- getRaster(shp_df = tcdshp$admin0, file_path = "C:/Users/whgu0734/Test/")
+## TEST2 <- getRaster(shp_df = MDGshp$admin0, file_path = "C:/Users/whgu0734/Test/")
