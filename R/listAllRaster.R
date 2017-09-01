86d251b4a3901ff33e1cfb4b16110108302710b8
Dan Pfeffer <daniel.pfeffer@well.ox.ac.uk>
2017-09-01 12:09:07 +0100

    added function allowing user to find all available rasters on geoserver closes #61

---
+++ R/listAllRaster.R
@@ -0,0 +1,36 @@
+listAllRaster <- function(printed = TRUE){
+
+  #query the geoserver to return xml containing a list of all available rasters & convert this to a list
+  xml <- XML::xmlParse("http://map-prod3.ndph.ox.ac.uk:8080/geoserver/ows?service=wms&version=1.3.0&request=GetCapabilities")
+  xml_data <- XML::xmlToList(xml)
+
+  #subset this list to only include list of rasters
+  layers <- xml_data$Capability$Layer[names(xml_data$Capability$Layer)=="Layer"]
+
+  #extract raster metadata from layers list & turn this into dataframe
+  codes <- unname(lapply(layers, "[[", 1))
+  title <- unname(lapply(layers, "[[", 2))
+  abstracts <- unname(lapply(layers, "[[", 3))
+
+  available_rasters <- data.frame(cbind("raster_code" = codes, "title" = title, "abstract" = abstracts))
+
+  html2text <- function(htmlString) {
+    htmlString <-  gsub("<small>", ":", htmlString)
+    return(gsub("<.*?>", "", htmlString))
+  }
+
+  available_rasters$raster_code <- sub("Explorer:", "", available_rasters$raster_code)
+  available_rasters$title_extended <- sub("^.*:","",html2text(available_rasters$title))
+  available_rasters$title <- sub(":.*$","",html2text(available_rasters$title))
+  available_rasters$abstract <- html2text(available_rasters$abstract)
+
+  available_rasters <- dplyr::select(available_rasters, 1,2,4,3)
+
+  if(printed == TRUE){
+    message("Rasters Available for Download: \n ",paste(available_rasters$title_extended, collapse = " \n "))
+  }
+
+  return(available_rasters)
+}
+z <- listAllRaster()
+
