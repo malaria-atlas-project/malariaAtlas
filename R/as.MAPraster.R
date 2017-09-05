59adb4dd806e9a4bf36a05844e901a0b7712b040
Dan Pfeffer <daniel.pfeffer@well.ox.ac.uk>
2017-09-05 09:14:19 +0100

    a bunch of fiddling on getRaster etc. hopefully closes issues but will manually tag this commit...

--- R/as.MAPraster.R
+++ R/as.MAPraster.R
@@ -1,7 +1,36 @@
-as.MAPraster <- function(raster){
+as.MAPraster <- function(raster_object){
 
-  raster_df <- as(raster, "SpatialPixelsDataFrame")
+  if("RasterLayer" %in% class(raster_object)){
+
+  raster_df <- as(raster_object, "SpatialPixelsDataFrame")
   raster_df <- as.data.frame(raster_df)
   class(raster_df) <- c(class(raster_df), "MAPraster")
   return(raster_df)
-}
+  }else if("RasterBrick"%in% class(raster_object)){
+    raster_df <- as(raster_object, "SpatialPixelsDataFrame")
+    raster_df <- as.data.frame(raster_df)
+    raster_columns <- names(raster_object)[!names(raster_object) %in% c("x", "y")]
+
+    mini_df <- function(column, df){
+      df <- df[,c(paste(column),"x","y")]
+    }
+
+
+    raster_df <- lapply(X = raster_columns, FUN = mini_df, df = raster_df)
+    raster_df <- lapply(raster_df, rbind)
+
+    raster_df <- gather(raster_df, raster_columns, c("x","y"))
+
+    class(raster_df) <- c(class(raster_df), "MAPraster")
+
+
+
+
+  }else if ("list" %in% class(raster_object))
+    res_1 <- raster_object[[1]]
+  res_2 <- raster_object[[2]]
+
+
+
+
+    }
--- R/getRaster.R
+++ R/getRaster.R
@@ -1,85 +1,85 @@
-getRaster <- function(surface = "PfPR2010", shp = NULL, bbox = NULL, file_path = tempdir(), format = "rasterlayer"){
+getRaster <- function(surface = "PfPR2-10", shp = NULL, bbox = NULL, file_path = tempdir(), format = "raster"){
 
-  if(!is.null(bbox)){
-    bbox <- bbox
-  }else if (!is.null(shp)){
+  ## if bbox is not defined by user, use sp::bbox to define this from provided shapefile
+  if(is.null(bbox)&!is.null(shp)){
   bbox <- sp::bbox(shp)
   }
 
-  if(surface == "PfPR2010"){
-    raster_code <- "ST_PR_mean"
-  }else if(surface == "PvPR2010"){
-    raster_code <- "PvPR_mean_2010"
-  }else if(surface == "Pk2016"){
-    raster_code <- "Pk_SEAsia_masked_280216"
-  }else if(surface == "Pf Limits 2010"){
-    raster_code <- "Pf_Limits_2010_decomp_tiled"
-  }else if(surface == "funestus"){
-    raster_code <- "Anophele funestus"
-  }else if(surface == "G6PDd2010"){
-    raster_code <- "G6PDd_allele_freq"
-  }else if(surface == "Duffy2010"){
-    raster_code <- "duffy_neg_tiled"
-  }else if (surface == "Africa_ACT_2000-2015"){
-    raster_code <- "africa-act-2000-2015"
-  }else if (surface == "Africa_Pf_Incidence_2000-2015"){
-    raster_code <- "africa-inc-2000-2015"
-  }
-
-  xml_rqst <- paste("<?xml version=\"1.0\" encoding=\"UTF-8\"?><GetCoverage version=\"1.0.0\" service=\"WCS\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://www.opengis.net/wcs\" xmlns:ows=\"http://www.opengis.net/ows/1.1\" xmlns:gml=\"http://www.opengis.net/gml\" xmlns:ogc=\"http://www.opengis.net/ogc\" xsi:schemaLocation=\"http://www.opengis.net/wcs http://schemas.opengis.net/wcs/1.0.0/getCoverage.xsd\">
-  <sourceCoverage>Explorer:",paste(raster_code),"</sourceCoverage>
-  <domainSubset>
-    <spatialSubset>
-      <gml:Envelope srsName=\"EPSG:4326\">
-        <gml:pos>",paste(bbox[,1], collapse = " "),"</gml:pos>
-        <gml:pos>",paste(bbox[,2], collapse = " "),"</gml:pos>
-      </gml:Envelope>
-      <gml:Grid dimension=\"2\">
-        <gml:limits>
-          <gml:GridEnvelope>
-            <gml:low>0 0</gml:low>
-            <gml:high>288 230</gml:high>
-          </gml:GridEnvelope>
-        </gml:limits>
-        <gml:axisName>x</gml:axisName>
-        <gml:axisName>y</gml:axisName>
-      </gml:Grid>
-    </spatialSubset>
-  </domainSubset>
-  <output>
-    <crs>EPSG:4326</crs>
-    <format>GeoTIFF</format>
-  </output>
-</GetCoverage>", sep = "")
+  #bbox[,1] <- substr(bbox[,1], 1,5)
+  #bbox[,2] <- substr(bbox[,2], 1,5)
+  ## return all available rasters and use this df to define raster codes for specifed 'Surfaces'
+  available_rasters <- listAllRaster(printed = FALSE)
+  raster_code_list <- available_rasters$raster_code[match(surface, available_rasters$title)]
 
   rstdir <- file.path(file_path,"getRaster")
   dir.create(rstdir, showWarnings = FALSE)
-  temp_rst <- tempfile(pattern = paste(surface,"_",sep = ""), tmpdir = rstdir, fileext = ".tiff")
 
-  r <- httr::POST(url = "http://map-prod3.ndph.ox.ac.uk/geoserver/wps",
-            body = xml_rqst,
-            httr::write_disk(temp_rst, overwrite = TRUE))
+  message("Downloading the following rasters: \n", paste(available_rasters$title[match(raster_code_list, available_rasters$raster_code)], collapse = "\n"))
 
-  if(!"image/tiff" %in% r$headers$`content-type`){
-    stop("Raster download error - check surface is available for specified extent at map.ox.ac.uk/explorer.")
+  download_rst <- function(raster_code, bbox, file_path){
+
+    rst_path <- file.path(file_path,"getRaster",paste(available_rasters$title[match(raster_code,available_rasters$raster_code)],"_",paste(substr(bbox,1,5),collapse = "_"),"_",Sys.Date(),".tiff",sep = ""))
+    rst_URL <- paste("http://map-prod3.ndph.ox.ac.uk/geoserver/Explorer/ows?service=WCS&version=2.0.1&request=GetCoverage&format=image/geotiff&coverageid=",raster_code,"&subset=Long(",paste(bbox[1,],collapse = ","),")&subset=Lat(",paste(bbox[2,],collapse = ","),")", sep = "")
+    r <- httr::GET(rst_URL, httr::write_disk(paste(rst_path), overwrite = TRUE))
+
+    if(!"image/geotiff" %in% r$headers$`content-type`){
+      file.remove(rst_path)
+      stop("Raster download error - check ",raster_code, " surface is available for specified extent at map.ox.ac.uk/explorer.")
+   message(rst_URL)
+    }
   }
 
+ #download rasters to designated file_path (tempdir as default)
+  invisible(sapply(X = raster_code_list, FUN = download_rst, bbox = bbox, file_path = file_path))
 
-  rst_dl <- raster::raster(temp_rst)
+  #create vector of filenames for rasters downloaded in the current query
+  newrst <- grep(paste(surface,"_",paste(substr(bbox,1,5),collapse = "_"),"_",Sys.Date(),collapse = "|", sep = ""),dir(rstdir), value = TRUE)
 
-  if(!is.null(shp)){
-    rst_dl <- raster::mask(rst_dl, shp)
-  }
-
-  if(tolower(format) == "rasterlayer"){
+  # Return error if new rasters are not found
+  if(length(newrst)==0){
+    stop("Raster download error: check surface and/or bbox are specified correctly")
+  # If only one new raster is found, read this in
+  }else if(length(newrst)==1){
+    rst_dl <- raster::raster(file.path(rstdir, newrst))
+    if(!is.null(shp)){
+      rst_dl <- mask(rst_dl, shp)
+    }
     return(rst_dl)
-  } else if(tolower(format) == "df"){
-    rst_spdf <- as(rst_dl, "SpatialPixelsDataFrame")
-    rst_df <- as.data.frame(rst_spdf)
-    class(rst_df) <- c(class(rst_df), "MAPraster")
-    return(rst_df)
+  #if more than one raster is found we want a raster stack - but only for rasters with the same resolution
+  }else if(length(newrst)>1){
+    #read in multiple rasters into a list
+    rst_list <- lapply(file.path(rstdir, newrst), raster::raster)
+    # create a dataframe with information on the resolution of each raster
+    rst_list_index <- data.frame(cbind("resolution" = lapply(rst_list, raster::res),
+                                       "raster_name" = lapply(rst_list, names),
+                                       "res_id" = as.numeric(factor(as.character(lapply(rst_list, raster::res))))))
+
+    #check whether more than one resolution is present in downloaded rasters
+    #if only one resolution is present, create a raster stack of all downloaded rasters
+    if(length(unique(rst_list_index$res_id))==1){
+      rst_stk <- stack(rst_list)
+      if(!is.null(shp)){
+        rst_stk <- mask(rst_stk, shp)
+      }
+      return(rst_stk)
+      #if not then we want to return a list of raster stacks - one for each resolution present in the index dataframe above.
+    }else if (length(unique(rst_list_index$res_id))!=1){
+      #create empty list
+      stk_list <- list()
+
+      #for each unique raster resolution create a raster stack and store this in stk_list
+      for(i in 1:length(unique(rst_list_index$res_id))){
+        stk_list[i] <- stack(rst_list[rst_list_index$res_id == i])
+      }
+        if(!is.null(shp)){
+          stk_list<- lapply(X =stk_list, FUN = mask, mask = shp)
+        }
+        return(stk_list)
+        }
+    }
   }
-}
+
 
 ## TEST_SHP <- getShp(ISO = "MDG")
 ## TEST <- getRaster(shp = TEST_SHP)
+## TEST2 <- getRaster(shp = TEST_SHP, surface = c("Pv Endemicity","Pf Spatial Limits"))
