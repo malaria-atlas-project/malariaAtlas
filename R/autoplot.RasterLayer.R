4cda2ee72b702411d60a41e8773de5f13f3b7dd6
Dan Pfeffer <daniel.pfeffer@well.ox.ac.uk>
2017-09-29 17:07:37 +0100

    removed old temporary file and renamed a file

--- R/autoplot.RasterLayer.R
+++
@@ -1,7 +0,0 @@
-autoplot.RasterLayer <- function(object, boundaries = NULL, shp_df = NULL, legend = ""){
-
-  object <- as.MAPraster(object)
-
-  autoplot(object = object, boundaries = boundaries, shp_df = shp_df, legend = legend)
-
-}
