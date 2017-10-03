06466c119d704697129e5a27684b842934f856ec
Dan Pfeffer <daniel.pfeffer@well.ox.ac.uk>
2017-10-03 17:17:48 +0100

    added citation to listAllRaster dataframe as well as a placeholder for publication_year (currenty based on keyword 'category' but will change this to new keyword later.

--- R/listAllRaster.R
+++ R/listAllRaster.R
@@ -19,25 +19,19 @@ listAllRaster <- function(printed = TRUE){
   #subset this list to only include list of rasters
   layers <- xml_data$Capability$Layer[names(xml_data$Capability$Layer)=="Layer"]
 
-  #extract raster metadata from layers list & turn this into dataframe
-  codes <- unname(lapply(layers, "[[", 1))
-  title <- unname(lapply(layers, "[[", 2))
-  abstracts <- unname(lapply(layers, "[[", 3))
-
-  available_rasters <- data.frame(cbind("raster_code" = codes, "title" = title, "abstract" = abstracts))
-
   #define small function to remove html tags from raster titles
   html2text <- function(htmlString) {
     htmlString <-  gsub("<small>", ":", htmlString)
     return(gsub("<.*?>", "", htmlString))
   }
 
-  #adjust text in various dataframe columns & order these as desired
-  available_rasters$raster_code <- sub("Explorer:", "", available_rasters$raster_code)
-  available_rasters$title_extended <- sub("^.*:","",html2text(available_rasters$title))
-  available_rasters$title <- sub(":.*$","",html2text(available_rasters$title))
-  available_rasters$abstract <- html2text(available_rasters$abstract)
-  available_rasters <- dplyr::select(available_rasters, 1,2,4,3)
+  #extract raster metadata from layers list & turn this into dataframe
+  available_rasters <- data.frame(cbind("raster_code" = unname(lapply(layers, function(x){sub("^Explorer:", "", x[["Name"]])})),
+                                        "title"= sub(":.*$", "", html2text(unname(lapply(layers, function(x){x[["Title"]]})))),
+                                        "title_extended" = sub("^.*:", "", html2text(unname(lapply(layers, function(x){x[["Title"]]})))),
+                                        "abstract" = html2text(unname(lapply(layers, function(x){x[["Abstract"]]}))),
+                                        "citation"= unname(lapply(layers, function(x){x[["Attribution"]][["Title"]]})),
+                                        "pub_year" = unname(lapply(layers, function(x){sub("^category:","",grep("^category:",unname(unlist(x[["KeywordList"]])), value = TRUE))}))))
 
   #print out message of long raster names
   if(printed == TRUE){
