92c7f9901cd5c9c3232eb5c3d5a5091bd676ec2d
Daniel Pfeffer <daniel.pfeffer@bdi.ox.ac.uk>
2017-12-21 09:53:32 +1000

    removing install lines of tidyverse

--- R/listAll.R
+++ R/listAll.R
@@ -7,12 +7,6 @@
 #' @examples
 #' listAll()
 #' @export listAll
-#'
-#'
-#'
-
-install.packages("tidyverse", repos = "https://cran.ma.imperial.ac.uk/")
-
 
 listAll <- function(printed = TRUE) {
   message("Creating list of countries for which PR data is available, please wait...")
