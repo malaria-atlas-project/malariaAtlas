64d59bfc9308e3f43a9cc6200d25068acf2257e1
Tim Lucas <timcdlucas@gmail.com>
2018-03-20 12:12:25 +0000

    Fix some paths in getSHP.

--- R/getShp.R
+++ R/getShp.R
@@ -64,12 +64,12 @@ getShp <- function(country = NULL,
     stop("Invalid country/ISO definition, use is_available() OR listShp() to confirm country spelling and/or ISO code.")
   }
 
-  if(admin_level=="both"){
-  admin_num <- c(0,1)
-  }else if (admin_level=="admin1"){
-  admin_num <- 1
-  }else if (admin_level=="admin0"){
-  admin_num <- 0
+  if(admin_level == "both"){
+    admin_num <- c(0, 1)
+  } else if (admin_level == "admin1"){
+    admin_num <- 1
+  } else if (admin_level == "admin0"){
+    admin_num <- 0
   }
 
   # if lat and long are provided, define extent from lat-longs
@@ -117,43 +117,43 @@ getShp <- function(country = NULL,
   URL_filter <- paste("&bbox=",paste(c(extent[2,1],extent[1,1],extent[2,2],extent[1,2]),collapse = ","), sep = "")
   #otherwise by default use object country names to download shapefiles for these countries only via cql_filter.
   }else{
-  URL_filter <- paste("&cql_filter=COUNTRY_ID%20IN%20(",paste("%27",country_input,"%27",collapse = ",", sep = ""),")", sep = "")
+  URL_filter <- paste("&cql_filter=country_id%20IN%20(",paste("%27",country_input,"%27",collapse = ",", sep = ""),")", sep = "")
   }
 
   #define which admin levels are queried and return as a list full geoserver query URL
   base_URL <- "https://map.ox.ac.uk/geoserver/ows?service=wfs&version=2.0.0&request=GetFeature&outputFormat=shape-zip&srsName=EPSG:4326"
   if(tolower(admin_level) == "admin0"){
-    URL_input <- list("admin0" = paste(base_URL,"&TypeName=admin0_map_2013",paste(URL_filter), sep = ""))
+    URL_input <- list("admin0" = paste(base_URL,"&TypeName=mapadmin_0_2013",paste(URL_filter), sep = ""))
   } else if(tolower(admin_level) == "admin1"){
-    URL_input <- list("admin1" = paste(base_URL,"&TypeName=admin1_map_2013",paste(URL_filter), sep = ""))
+    URL_input <- list("admin1" = paste(base_URL,"&TypeName=mapadmin_1_2013",paste(URL_filter), sep = ""))
   } else if(tolower(admin_level) == "both"){
-    URL_input <- list("admin0" = paste(base_URL,"&TypeName=admin0_map_2013",paste(URL_filter), sep = ""),
-                      "admin1" = paste(base_URL,"&TypeName=admin1_map_2013",paste(URL_filter), sep = ""))
+    URL_input <- list("admin0" = paste(base_URL,"&TypeName=mapadmin_0_2013",paste(URL_filter), sep = ""),
+                      "admin1" = paste(base_URL,"&TypeName=mapadmin_1_2013",paste(URL_filter), sep = ""))
   }
-
-Shp_polygon <- lapply(URL_input, downloadShp)
-
-if(admin_level!="both"){
-  Shp_polygon <- Shp_polygon[[paste(admin_level)]]
-  if("sum" %in% names(Shp_polygon)|"mean" %in% names(Shp_polygon)|"LSMS.agri" %in% names(Shp_polygon)){
-    Shp_polygon <- Shp_polygon[,!names(Shp_polygon)%in% c("sum", "mean","LSMS.agri")]
-  }
-}else if (admin_level == "both"){
-  Shp_polygon <- sp::rbind.SpatialPolygonsDataFrame(Shp_polygon$admin0[names(Shp_polygon$admin1)], Shp_polygon$admin1)
-}
-
-Shp_polygon$country_level <- paste(Shp_polygon$COUNTRY_ID,"_",Shp_polygon$ADMN_LEVEL,sep = "")
-
-if("all" %in% tolower(ISO)){
-  .malariaAtlasHidden$all_polygons <- Shp_polygon
-  } else if(is.null(extent)){
-    if(!exists("stored_polygons", envir = .malariaAtlasHidden)){
-    .malariaAtlasHidden$stored_polygons <- Shp_polygon
-    }else if(exists("stored_polygons", envir = .malariaAtlasHidden)){
-      new_shps <- Shp_polygon[!Shp_polygon$country_level %in% unique(.malariaAtlasHidden$stored_polygons$country_level),]
-      .malariaAtlasHidden$stored_polygons <- sp::rbind.SpatialPolygonsDataFrame(.malariaAtlasHidden$stored_polygons, new_shps[names(new_shps)[names(new_shps)%in% names(.malariaAtlasHidden$stored_polygons)]])
+  
+  Shp_polygon <- lapply(URL_input, downloadShp)
+  
+  if(admin_level!="both"){
+    Shp_polygon <- Shp_polygon[[paste(admin_level)]]
+    if("sum" %in% names(Shp_polygon)|"mean" %in% names(Shp_polygon)|"LSMS.agri" %in% names(Shp_polygon)){
+      Shp_polygon <- Shp_polygon[,!names(Shp_polygon)%in% c("sum", "mean","LSMS.agri")]
     }
-}
+  }else if (admin_level == "both"){
+    Shp_polygon <- sp::rbind.SpatialPolygonsDataFrame(Shp_polygon$admin0[names(Shp_polygon$admin1)], Shp_polygon$admin1)
+  }
+  
+  Shp_polygon$country_level <- paste(Shp_polygon$COUNTRY_ID,"_",Shp_polygon$ADMN_LEVEL,sep = "")
+  
+  if("all" %in% tolower(ISO)){
+    .malariaAtlasHidden$all_polygons <- Shp_polygon
+    } else if(is.null(extent)){
+      if(!exists("stored_polygons", envir = .malariaAtlasHidden)){
+      .malariaAtlasHidden$stored_polygons <- Shp_polygon
+      }else if(exists("stored_polygons", envir = .malariaAtlasHidden)){
+        new_shps <- Shp_polygon[!Shp_polygon$country_level %in% unique(.malariaAtlasHidden$stored_polygons$country_level),]
+        .malariaAtlasHidden$stored_polygons <- sp::rbind.SpatialPolygonsDataFrame(.malariaAtlasHidden$stored_polygons, new_shps[names(new_shps)[names(new_shps)%in% names(.malariaAtlasHidden$stored_polygons)]])
+      }
+  }
 
   if(tolower(format)=="spatialpolygon"){
     return(Shp_polygon)
