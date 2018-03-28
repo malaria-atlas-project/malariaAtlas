4ce90f4c940284baa7d4aa029360d58cf01993d4
Tim Lucas <timcdlucas@gmail.com>
2018-03-28 15:51:52 +0100

    Fix some docs names.

--- R/getRaster.R
+++ R/getRaster.R
@@ -19,14 +19,16 @@
 #' autoplot_MAPraster(MDG_PfPR2_10)
 #'
 #' # Download global raster of G6PD deficiency from Howes et al 2012.
-#' G6PDd_global <- getRaster(surface = "G6PD Deficiency")
+#' G6PDd_global <- getRaster(surface = "G6PD Deficiency Allele Frequency")
 #' autoplot_MAPraster(G6PDd_global)
 #'
 #' # Download a temporal raster by range
 #' MDG_PfPR2_10_range <- getRaster(surface = "Plasmodium falciparum PR2-10", shp = MDG_shp, year = 2012:2015)
 #'
 #' # Download a mix of rasters
-#' MDG_rasters <- getRaster(surface = c("Plasmodium falciparum PR2-10", 'Pf Incidence', 'Pf Support'),
+#' MDG_rasters <- getRaster(surface = c("Plasmodium falciparum PR2-10", 
+#'                                      'Plasmodium falciparum Incidence', 
+#'                                      'Plasmodium falciparum Support'),
 #'                          shp = MDG_shp, year = list(2009:2012, 2005:2007, NA))
 #' p <- autoplot_MAPraster(MDG_tvr_tvr_s)
 #'
