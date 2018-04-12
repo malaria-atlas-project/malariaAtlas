8f2495220b49d849310010c600b024d69cbab86d
Tim Lucas <timcdlucas@gmail.com>
2018-04-12 21:52:11 +0100

    Add test that prints an image to see if that breaks CI.

--- tests/testthat/test_autoplot.MAPraster.R
+++ tests/testthat/test_autoplot.MAPraster.R
@@ -3,12 +3,27 @@ context('Test Raster autoplotting is working.')
 # add more types of rasters to plot
 
 MDG_shp <- getShp(ISO = "MDG", admin_level = "admin0")
+MDG_PfPR2_10_range <- getRaster(surface = "Plasmodium falciparum PR2-10", shp = MDG_shp, year = 2012:2015)
 
 test_that('Plotting works for 4 years of the same raster', {
 #time varying range
-MDG_PfPR2_10_range <- getRaster(surface = "Plasmodium falciparum PR2-10", shp = MDG_shp, year = 2012:2015)
+
 p <- autoplot_MAPraster(MDG_PfPR2_10_range, printed = FALSE) # this works
 pp <- autoplot(as.MAPraster(MDG_PfPR2_10_range), printed = FALSE) # This goes funky. Need invisible()
+expect_true(inherits(MDG_PfPR2_10_range, 'RasterBrick'))
+expect_true(inherits(p, "list"))
+expect_true(inherits(pp, "list"))
+expect_true(all(sapply(p, inherits, "gg")))
+expect_true(all(sapply(pp, inherits, "gg")))
+expect_true(length(p) == 4)
+expect_true(length(pp) == 4)
+
+})
+
+
+test_that('Printing plot works', {
+p <- autoplot_MAPraster(MDG_PfPR2_10_range, printed = TRUE) # this works
+pp <- autoplot(as.MAPraster(MDG_PfPR2_10_range), printed = TRUE) # This goes funky. Need invisible()
 expect_true(inherits(MDG_PfPR2_10_range, 'RasterBrick'))
 expect_true(inherits(p, "list"))
 expect_true(inherits(pp, "list"))
