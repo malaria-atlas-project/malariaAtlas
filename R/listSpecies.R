339196fadf0b8f130d3a661b13f7cfaa0531cf9b
Suzanne Keddie <suzannek@ndph.ox.ac.uk>
2018-08-06 16:02:17 +0100

    Function that returns a list of species with available MAP data

---
+++ R/listSpecies.R
@@ -0,0 +1,55 @@
+#' list all species which have occurrence data within the MAP database
+#' 
+#' \code{listSpecies} lists all species occurrence data available to download from the Malaria Atlas Project database
+#' @param printed should the list be printed to the database
+#' @return \code {listSpecies} returns a data.frame detailing the following information for each species available to download from the Malaria Atlas Project database.
+#' 
+#' \enumerate{
+#' \item \code{species} string detailing species
+#' }
+#' #' @examples
+#' \donttest{
+#' available_species <- listSpecies()
+#' }
+#' @export listSpecies
+#' 
+listSpecies <- function(printed = T){
+  message("Downloading list of available species, please wait...")
+  
+  if(exists('available_species_stored', envir = .malariaAtlasHidden)){
+    available_species <- .malariaAtlasHidden$available_species_stored
+  
+ 
+  if(printed == TRUE){
+     message("Species with available data: \n ",paste(paste(available_species$species, " (",available_species$country, ")", sep = ""), collapse = " \n ")) 
+  }
+    
+  return(invisible(available_species))
+  
+  } else {
+    
+    URL <- 
+      "https://map.ox.ac.uk/geoserver/Explorer/ows?service=wfs&version=2.0.0&request=GetFeature&outputFormat=csv&TypeName=Anopheline_Data"
+    
+    columns <- 
+      "&PROPERTYNAME=species,country"
+    
+    available_species <- unique(utils::read.csv(paste(URL, columns, sep = ""), encoding = "UTF-8")[,c("species", "country")]) 
+    available_species <- dplyr::select(available_species, species, country)
+
+    
+  if(printed == TRUE){
+    message("Species with availale data: \n ",paste(paste(available_species$species, " (",available_species$country, ")", sep = ""), collapse = " \n "))
+  }
+  
+  .malariaAtlasHidden$available_species_stored <- available_species
+  
+  
+  return(invisible(available_species))
+  }
+}
+#listSpecies()
+#xx <- listSpecies(printed = F)
+
+  
+  
