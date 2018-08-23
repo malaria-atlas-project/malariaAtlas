c878e59d7b29905f4b7c10772ce63c0623cfcfd8
Suzanne Keddie <suzannek@ndph.ox.ac.uk>
2018-08-23 09:52:47 +0100

    negligible

--- R/listSpecies.R
+++ R/listSpecies.R
@@ -1,18 +1,20 @@
-#' list all species which have occurrence data within the MAP database
+#' list all species which have occurrence data within the MAP database.
 #' 
-#' \code{listSpecies} lists all species occurrence data available to download from the Malaria Atlas Project database
-#' @param printed should the list be printed to the database
-#' @return \code {listSpecies} returns a data.frame detailing the following information for each species available to download from the Malaria Atlas Project database.
+#' \code{listSpecies} lists all species occurrence data available to download from the Malaria Atlas Project database.
+#' @param printed should the list be printed to the database.
+#' @return \code{listSpecies} returns a data.frame detailing the following information for each species available to download from the Malaria Atlas Project database.
 #' 
 #' \enumerate{
 #' \item \code{species} string detailing species
 #' }
+#' 
 #' @examples
 #' \donttest{
 #' available_species <- listSpecies()
 #' }
-#' @export listSpecies
 #' 
+#' @export listSpecies
+
 listSpecies <- function(printed = TRUE){
   message("Downloading list of available species, please wait...")
   
@@ -37,6 +39,9 @@ listSpecies <- function(printed = TRUE){
     available_species <- unique(utils::read.csv(paste(URL, columns, sep = ""), encoding = "UTF-8")[,c("species_plain", "country")]) 
     available_species <- dplyr::select(available_species, species_plain, country)
 
+    #Just to avoid visible binding notes
+    species_plain <- species_plain <- permissions_info <- NULL #### check this is right, any others needed?
+    country <- country <- permissions_info <- NULL
     
     if(printed == TRUE){
      message("Species with availale data: \n ",paste(paste(available_species$species, " (",available_species$country, ")", sep = ""), collapse = " \n "))
