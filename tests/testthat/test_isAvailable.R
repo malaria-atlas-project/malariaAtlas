5b653c2735897032ada0445b8b5fb7734cf7f4af
Suzanne Keddie <suzannek@ndph.ox.ac.uk>
2018-08-23 09:54:30 +0100

    Updated in line with new function

--- tests/testthat/test_isAvailable.R
+++ tests/testthat/test_isAvailable.R
@@ -1,38 +1,56 @@
 # isAvailable() tests
 
-context("Using isAvailable to check whether there are PR points for specified locations in the MAP database")
+context("Using isAvailable to check whether there are PR and Vector points for specified locations in the MAP database")
 
 
 # specify the example sets that I want to be available to test the returned data frame
 
 
-test_that("correct error messages are returned for various combinations of countries", {
+test_that("correct error messages are returned for various combinations of countries and sourcedata", {
 #one right
-  expect_message(isAvailable(country = "Madagascar"), "PR points are available for Madagascar")
+  expect_message(isAvailable(country = "Madagascar", sourcedata = "pr points"), "PR points are available for Madagascar")
+  expect_message(isAvailable(country = "Myanmar", sourcedata = "vector points"), "Vector points are available for Myanmar")
 #one right, one wrong
-  expect_message(isAvailable(country = c("Madagascar","Australia")), "PR points are available for Madagascar")
-  expect_warning(isAvailable(country = c("Madagascar","Australia")), "Data not found for 'Australia', use listPoints()")
+  expect_message(isAvailable(country = c("Madagascar","Australia"), sourcedata = "pr points"), "PR points are available for Madagascar")
+  expect_warning(isAvailable(country = c("Madagascar","Australia"), sourcedata = "pr points"), "Data not found for 'Australia', use listPoints()")
+  expect_message(isAvailable(country = c("Brazil","Libya"), sourcedata = "vector points"), "Vector points are available for Brazil")
+  expect_warning(isAvailable(country = c("Brazil","Libya"), sourcedata = "vector points"), "Data not found for 'Libya', use listPoints()")
 #one right, one mispelled
-  expect_message(isAvailable(country = c("Madagascar","Ngeria")), "PR points are available for Madagascar")
-  expect_warning(isAvailable(country = c("Madagascar","Ngeria")), "Data not found for 'Ngeria', did you mean Nigeria")
+  expect_message(isAvailable(country = c("Madagascar","Ngeria"), sourcedata = "pr points"), "PR points are available for Madagascar")
+  expect_warning(isAvailable(country = c("Madagascar","Ngeria"), sourcedata = "pr points"), "Data not found for 'Ngeria', did you mean Nigeria")
+  expect_message(isAvailable(country = c("Brazil","Ngeria"), sourcedata = "vector points"), "Vector points are available for Brazil")
+  expect_warning(isAvailable(country = c("Brazil","Ngeria"), sourcedata = "vector points"), "Data not found for 'Ngeria', did you mean Nigeria")
 #one right, one wrong, one mispelled
-  expect_message(isAvailable(country = c("Madagascar","Australia","Ngeria")), "PR points are available for Madagascar")
-  expect_warning(isAvailable(country = c("Madagascar","Australia","Ngeria")), "Data not found for 'Australia', use listPoints()")
-  expect_warning(isAvailable(country = c("Madagascar","Australia","Ngeria")), "Data not found for 'Ngeria', did you mean Nigeria")
+  expect_message(isAvailable(country = c("Madagascar","Australia","Ngeria"), sourcedata = "pr points"), "PR points are available for Madagascar")
+  expect_warning(isAvailable(country = c("Madagascar","Australia","Ngeria"), sourcedata = "pr points"), "Data not found for 'Australia', use listPoints()")
+  expect_warning(isAvailable(country = c("Madagascar","Australia","Ngeria"), sourcedata = "pr points"), "Data not found for 'Ngeria', did you mean Nigeria")
+  expect_message(isAvailable(country = c("Brazil","Libya","Ngeria"), sourcedata = "vector points"), "Vector points are available for Brazil")
+  expect_warning(isAvailable(country = c("Brazil","Libya","Ngeria"), sourcedata = "vector points"), "Data not found for 'Libya', use listPoints()")
+  expect_warning(isAvailable(country = c("Brazil","Libya","Ngeria"), sourcedata = "vector points"), "Data not found for 'Ngeria', did you mean Nigeria")
+  
 #one wrong
-  expect_error(isAvailable(country = "Australia"), "Specified location not found, see below comments:")
-  expect_error(isAvailable(country = "Australia"), "Data not found for 'Australia', use listPoints()")
+  expect_error(isAvailable(country = "Australia", sourcedata = "pr points"), "Specified location not found, see below comments:")
+  expect_error(isAvailable(country = "Australia", sourcedata = "pr points"), "Data not found for 'Australia', use listPoints()")
+  expect_error(isAvailable(country = "Libya", sourcedata = "vector points"), "Specified location not found, see below comments:")
+  expect_error(isAvailable(country = "Libya", sourcedata = "vector points"), "Data not found for 'Libya', use listPoints()")
 #one mispelled
-  expect_error(isAvailable(country = "Ngeria"), "Specified location not found, see below comments:")
-  expect_error(isAvailable(country = "Ngeria"), "Data not found for 'Ngeria', did you mean Nigeria")
+  expect_error(isAvailable(country = "Ngeria", sourcedata = "pr points"), "Specified location not found, see below comments:")
+  expect_error(isAvailable(country = "Ngeria", sourcedata = "pr points"), "Data not found for 'Ngeria', did you mean Nigeria")
+  expect_error(isAvailable(country = "Ngeria", sourcedata = "vector points"), "Specified location not found, see below comments:")
+  expect_error(isAvailable(country = "Ngeria", sourcedata = "vector points"), "Data not found for 'Ngeria', did you mean Nigeria")
 #one wrong, one mispelled
-  expect_error(isAvailable(country = c("Australia","Ngeria")), "Specified location not found, see below comments:")
-  expect_error(isAvailable(country = c("Australia","Ngeria")), "Data not found for 'Australia', use listPoints()")
-  expect_error(isAvailable(country = c("Australia","Ngeria")), "Data not found for 'Ngeria', did you mean Nigeria")
+  expect_error(isAvailable(country = c("Australia","Ngeria"), sourcedata = "pr points"), "Specified location not found, see below comments:")
+  expect_error(isAvailable(country = c("Australia","Ngeria"), sourcedata = "pr points"), "Data not found for 'Australia', use listPoints()")
+  expect_error(isAvailable(country = c("Australia","Ngeria"), sourcedata = "pr points"), "Data not found for 'Ngeria', did you mean Nigeria")
+  expect_error(isAvailable(country = c("Libya","Ngeria"), sourcedata = "vector points"), "Specified location not found, see below comments:")
+  expect_error(isAvailable(country = c("Libya","Ngeria"), sourcedata = "vector points"), "Data not found for 'Libya', use listPoints()")
+  expect_error(isAvailable(country = c("Libya","Ngeria"), sourcedata = "vector points"), "Data not found for 'Ngeria', did you mean Nigeria")
 #nonsense
-  expect_error(isAvailable(country = "XfUEC43"), "Specified location not found, see below comments:")
-  expect_error(isAvailable(country = "XfUEC43"), "Data not found for 'Xfuec43', use listPoints()")
-
+  expect_error(isAvailable(country = "XfUEC43", sourcedata = "pr points"), "Specified location not found, see below comments:")
+  expect_error(isAvailable(country = "XfUEC43", sourcedata = "pr points"), "Data not found for 'Xfuec43', use listPoints()")
+  expect_error(isAvailable(country = "XfUEC43", sourcedata = "vector points"), "Specified location not found, see below comments:")
+  expect_error(isAvailable(country = "XfUEC43", sourcedata = "vector points"), "Data not found for 'Xfuec43', use listPoints()")
+  
   })
 
 test_that("isAvailable returnes the proper data frame if full_results is specified", {
