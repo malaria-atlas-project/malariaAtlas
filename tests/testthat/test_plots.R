7a7e3ff431497f2599e4639f65a15d49bc39f2a7
Tim Lucas <timcdlucas@gmail.com>
2018-09-04 16:01:00 +0100

    Add more plotting tests.

--- tests/testthat/test_plots.R
+++ tests/testthat/test_plots.R
@@ -51,6 +51,41 @@ p5 <- autoplot(NGA_CMR_PR, facet = FALSE)
 expect_true(inherits(p4, 'gg'))
 expect_true(inherits(p5, 'gg'))
 
+
+p6 <- autoplot(NGA_CMR_PR, admin_level = 'admin1')
+expect_true(inherits(p6, 'gg'))
+p6 <- autoplot(NGA_CMR_PR, admin_level = 'both')
+expect_true(inherits(p6, 'gg'))
+
+
+})
+
+
+
+
+
+test_that('Plotting works for vectors', {
+  #time varying range
+  skip_on_cran()
+  
+  
+  PAK_vec <- getVecOcc(country = "Pakistan")
+  p1 <- autoplot(PAK_vec)
+  p2 <- autoplot(PAK_vec, facet = TRUE)
+  expect_true(inherits(p1, 'gg'))
+  expect_true(inherits(p2, 'gg'))
+  
+  PAK_shp <- getShp(ISO = "PAK", admin_level = c("admin0", "admin1"))
+  p3 <- autoplot(PAK_vec, shp_df = as.MAPshp(PAK_shp))
+  expect_true(inherits(p3, 'gg'))
+  
+  # Check all admin levels
+  p4 <- autoplot(PAK_vec, admin_level = 'admin1')
+  p5 <- autoplot(PAK_vec, admin_level = 'both')
+  expect_true(inherits(p4, 'gg'))
+  expect_true(inherits(p5, 'gg'))
+  
+  
 })
 
 
