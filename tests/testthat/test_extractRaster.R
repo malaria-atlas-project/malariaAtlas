01789669c3127486452ac900c5026efbd63771fa
Tim Lucas <timcdlucas@gmail.com>
2018-09-04 16:17:27 +0100

    Add tests for extract raster.

---
+++ tests/testthat/test_extractRaster.R
@@ -0,0 +1,13 @@
+
+context('Test extract raster')
+
+test_that('Extract raster basics works', {
+    
+  d <- data.frame(x = c(4, 8), y = c(13, 9))
+  data <- extractRaster(d,
+                        surface = 'Plasmodium falciparum PR2-10',
+                        year = 2015)
+  
+  expect_true(inherits(d, 'data.frame'))
+  expect_true(all(d$value < 1 & d$value > 0))
+}
