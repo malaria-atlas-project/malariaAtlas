3eb6b10da26512926b799e44b917ba73b7067d55
Tim Lucas <timcdlucas@gmail.com>
2018-09-06 13:02:30 +0100

    Formatting.

--- R/listData.R
+++ R/listData.R
@@ -21,7 +21,7 @@
 #'
 #' @export listData
 
-listData <- function(datatype = NULL, printed = TRUE,...){
+listData <- function(datatype, printed = TRUE, ...){
 
   if(!datatype %in% c('pr points', 'vector points', 'raster', 'shape')){
     stop("Please choose one of: \n datatype = \"pr points\"  \n datatype = \"vector points\" \n datatype = \"raster\" \n datatype = \"shape\"")
@@ -34,7 +34,7 @@ listData <- function(datatype = NULL, printed = TRUE,...){
   }else if(datatype == "raster"){
     listRaster(printed = printed)
   }else if(datatype == "shape"){
-    listShp(printed = printed,...)
+    listShp(printed = printed, ...)
   }
 
 }
