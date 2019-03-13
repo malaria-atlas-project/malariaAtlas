5adcd294692967fae4f042d5efabf27b8ea7e5b8
Tim Lucas <timcdlucas@gmail.com>
2019-03-13 22:28:55 +0000

    Remove extractRaster tests for now.

--- tests/testthat/test_extractRaster.R
+++ tests/testthat/test_extractRaster.R
@@ -2,7 +2,7 @@
 context('Test extract raster')
 
 test_that('Extract raster basics works', {
-  
+    
   #skip_on_cran()
   #d <- data.frame(x = c(4, 8), y = c(13, 9))
   #data <- extractRaster(d,
@@ -12,4 +12,7 @@ test_that('Extract raster basics works', {
   #expect_true(inherits(d, 'data.frame'))
   #expect_true(all(d$value < 1 & d$value > 0))
 })
+
+
+
 
