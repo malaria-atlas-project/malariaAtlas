60c404e971ea15c6dc0c4f877503909aca80a592
Tim Lucas <timcdlucas@gmail.com>
2019-08-02 09:52:09 +0100

    Remove filldhs test for now because its really complicated.

--- tests/testthat/test_fillDHScoordinates.R
+++ tests/testthat/test_fillDHScoordinates.R
@@ -1,40 +1,40 @@
 
-skip_if_no_auth <- function(config_path = "rdhs.json") {
+#skip_if_no_auth <- function(config_path = "rdhs.json") {
   
-  testthat::skip_on_cran()
+#  testthat::skip_on_cran()
   
-  have_cred_path <- file.exists(config_path)
-  if (!have_cred_path) {
-    skip("No authentication available")
-  }
+#  have_cred_path <- file.exists(config_path)
+#  if (!have_cred_path) {
+#    skip("No authentication available")
+#  }
   
-  skip_if_slow_API()
+#  skip_if_slow_API()
   
-}
+#}
 
 # The function fillDHScoordinates needs verification
 #   as there's no way around this, I can't see how to test this
 #   function yet.
 
-context("Test fillDHScoordinates")
+#context("Test fillDHScoordinates")
 
-test_that("data is downloaded as a data.frame", {
+#test_that("data is downloaded as a data.frame", {
   
-  skip_if_no_auth()
-  pf <- getPR(country = "all", species = "Pf")
+#  skip_if_no_auth()
+#  pf <- getPR(country = "all", species = "Pf")
   
-  missing <- nrow(pf %>% filter(is.na(positive)))
+#  missing <- nrow(pf %>% filter(is.na(positive)))
   
-  expect_error(
-    pf2 <- fillDHSCoordinates(pf),
-    NA
-  )
-  missing2 <- nrow(pf2 %>% filter(is.na(positive)))
+#  expect_error(
+#    pf2 <- fillDHSCoordinates(pf),
+#    NA
+#  )
+#  missing2 <- nrow(pf2 %>% filter(is.na(positive)))
   
-  expect_that(missing > missing2)
+#  expect_that(missing > missing2)
     
-})
-                           
+#})
+
 
                            
                                                       
