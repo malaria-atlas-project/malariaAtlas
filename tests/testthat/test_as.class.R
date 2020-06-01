390928d489f5045ade5d1ce36d8d3a47a5bd8efd
Tim Lucas <timcdlucas@gmail.com>
2020-06-01 08:48:19 +0100

    Remove a test that confirms that dplyr removes pr.points class.

--- tests/testthat/test_as.class.R
+++ tests/testthat/test_as.class.R
@@ -13,7 +13,7 @@ test_that('as.pr.points function works.', {
   d2 <- getPR(country = c("Nigeria", "Cameroon"), species = "Pf")
   d3 <- dplyr::filter(d2, year_start > 2010)
   
-  expect_false(inherits(d3, 'pr.points'))
+  #expect_false(inherits(d3, 'pr.points'))
   
   d4 <- as.pr.points(d3)
   expect_true(inherits(d4, 'pr.points'))
