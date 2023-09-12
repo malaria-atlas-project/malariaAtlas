752e0791de436516129afc7ec0f40bcb00666338
sarahmconnor <sarah.connor@telethonkids.org.au>
2023-09-12 15:20:46 +0800

    Updated listRaster function with dataset and API changes and added to onload

--- R/ows4r_utils.R
+++ R/ows4r_utils.R
@@ -1,3 +1,17 @@
+#' Get the workspace and version from a raster id.
+#'
+#' @param id The ID to parse.
+#' @return A list with the workspace and version.
+#' @keywords internal
+#'
+get_workspace_and_version_from_coverage_id <- function(id) {
+  parts <- unlist(strsplit(id, "__"))
+  workspace <- parts[1]
+  wcs_client <- getOption("malariaatlas.wcs_clients")[[workspace]]
+  version <- unlist(strsplit(parts[2], "_"))[1]
+  return(list(workspace = workspace, version = version))
+}
+
 #' Get the workspace and version from a wfs feature type id.
 #'
 #' @param id The ID to parse.
--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,22 @@
 .onLoad <- function(libname, pkgname) {
  assign(x = ".malariaAtlasHidden", value = new.env(), pos = parent.env(environment()))
+  
+  workspaces <- c("Malaria", "Interventions", "Blood_Disorders", "Accessibility", "Vector_Occurrence", "Explorer")
+  options("workspaces" = workspaces)
+  wms_clients_by_workspace <- list()
+  wcs_clients_by_workspace <- list()
+  wfs_clients_by_workspace <- list()
+  for (workspace in workspaces) {
+    wms_client <- ows4R::WMSClient$new(paste0("https://data-dev.malariaatlas.org/geoserver/", workspace, "/ows"), "1.3.0")
+    wms_clients_by_workspace[[workspace]] <- wms_client
+    wcs_client <- ows4R::WCSClient$new(paste0("https://data-dev.malariaatlas.org/geoserver/", workspace, "/ows"), "2.0.1")
+    wcs_clients_by_workspace[[workspace]] <- wcs_client
+    wfs_client <- ows4R::WFSClient$new(paste0("https://data-dev.malariaatlas.org/geoserver/", workspace, "/ows"), "2.0.0", logger = "INFO")
+    wfs_clients_by_workspace[[workspace]] <- wfs_client
+  }
+  options("malariaatlas.workspaces" = workspaces)
+  options("malariaatlas.wms_clients" = wms_clients_by_workspace)
+  options("malariaatlas.wcs_clients" = wcs_clients_by_workspace)
+  options("malariaatlas.wfs_clients" = wfs_clients_by_workspace)
+  
 }
