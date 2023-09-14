c42c7a2070829aaede19d4583bb342a3a31932ef
sarahmconnor <sarah.connor@telethonkids.org.au>
2023-09-14 16:38:42 +0800

    Merge conflict resolved that should have been included in previous merge commit

--- R/getShp.R
+++ R/getShp.R
@@ -5,14 +5,9 @@
 #' @param country string containing name of desired country, e.g. \code{ c("Country1", "Country2", ...)} OR \code{ = "ALL"} (use either \code{ISO} OR \code{country})
 #' @param ISO string containing ISO3 code for desired country, e.g. \code{c("XXX", "YYY", ...)} OR \code{ = "ALL"} (use either \code{ISO} OR \code{country})
 #' @param admin_level string specifying the administrative level for which shapefile are desired (only "admin0","admin1","admin2","admin3", or "all" accepted). N.B. Not all administrative levels are available for all countries. Use listShp to check which shapefiles are available. If an administrative level is requested that is not available, the closest available administrative level shapefiles will be returned.
-#' @param extent 2x2 matrix specifying the spatial extent within which polygons are desired. The first column has the minimum, the second the maximum values; rows 1 & 2 represent the x & y dimensions respectively (matrix(c("xmin", "ymin","xmax", "ymax"), nrow = 2, ncol = 2, dimnames = list(c("x", "y"), c("min", "max")))).
-#' Note: getShp downloads the entire polygon for any polygons falling within the extent.
-<<<<<<< HEAD
+#' @param extent 2x2 matrix specifying the spatial extent within which polygons are desired, as returned by sp::bbox() - the first column has the minimum, the second the maximum values; rows 1 & 2 represent the x & y dimensions respectively (matrix(c("xmin", "ymin","xmax", "ymax"), nrow = 2, ncol = 2, dimnames = list(c("x", "y"), c("min", "max")))).
 #' @param sf an sf (simple feature) object specifying the spatial extent within which polygons are desired
-#' @param format string specifying the desired format for the downloaded shapefile: either "spatialpolygon" or "df" (currently meaningless, soon to be deprecated)
-=======
 #' @param format deprecated argument. Please remove it from your code.
->>>>>>> e790317eafd0ae3164ff98a3d5dc73cdd48c2de5
 #' @param long longitude of a point location falling within the desired shapefile.
 #' @param lat latitude of a point location falling within the desired shapefile.
 #' @param version The admin unit dataset version to return. Is NULL by default, and if left NULL will just use the most recent version of admin unit data.
@@ -67,28 +62,12 @@ getShp <- function(country = NULL,
                    admin_level = c("admin0"),
                    format = NULL,
                    long = NULL,
-<<<<<<< HEAD
                    lat = NULL, 
                    version = NULL) {
-=======
-                   lat = NULL) {
   if (!is.null(format)) {
     lifecycle::deprecate_warn("1.5.0", "getShp(format)", details = "The argument 'format' has been deprecated. It will be removed in the next version. Admin boundaries will be correctly plotted using autoplot without the argument.")
   }
   
-  # Specify country_input (ISO3 code) for db query
-    available_admin <- listShp(printed = FALSE, admin_level= "admin0")
-  if(inherits(available_admin, 'try-error')){
-    message(available_admin)
-    return(available_admin)
-  }
->>>>>>> e790317eafd0ae3164ff98a3d5dc73cdd48c2de5
-  
-  if(!is.null(format)) {
-    message('Please Note: The format parameter will soon be deprecated. Currently, the option for spatialPolygon has already been removed, and the data
-            is always returned as a data.frame. Therefore, the format parameter currently is not used, and will soon be removed, so please stop using it.')
-  }
-
   available_admin <- listShp(printed = FALSE, admin_level= "admin0", version = version)
 
   if(all(!admin_level %in% c("admin0", "admin1", "admin2", "admin3", "all"))){
@@ -107,7 +86,7 @@ getShp <- function(country = NULL,
     stop("If you specify one of 'lat' or 'long', you must also specify the other.")
   }
   
-  if (sum(c(!is.null(extent), !is.null(sf), !is.null(lat) | !is.null(lat)), na.rm = TRUE)) {
+  if (sum(c(!is.null(extent), !is.null(sf), !is.null(lat) | !is.null(lat)), na.rm = TRUE) > 1) {
     stop("Can only specify one of: 'extent', 'sf' or 'lat' and 'long'. Please choose one.")
   }
   
@@ -133,7 +112,7 @@ getShp <- function(country = NULL,
     iso_list <-
       as.character(available_admin$iso[available_admin$name_0 %in% country])
   } else if (!is.null(lat) & !is.null(long)) {
-    latlong_extent <- sp::bbox(array(data.frame(long, lat)))
+    latlong_extent <- matrix(unlist(sf::st_bbox(array(data.frame(long, lat)))), ncol = 2)
     bbox_filter <- build_bbox_filter(latlong_extent)
   } else if (!is.null(extent)) {
     bbox_filter <- build_bbox_filter(extent)
@@ -149,79 +128,9 @@ getShp <- function(country = NULL,
     )
   }
 
-<<<<<<< HEAD
+
   #Getting features
-=======
-  if ("all"%in% tolower(admin_level)){
-    admin_num <- c(0, 1, 2, 3)
-  } else {
-    admin_num <- as.numeric(gsub("admin","", admin_level))
-  } 
 
-  # if lat and long are provided, define extent from lat-longs
-
-  if (!is.null(lat) & !is.null(long)) {
-    extent <- extent <- matrix(unlist(sf::st_bbox(array(data.frame(long, lat)))), ncol = 2)
-  }
-
-  #treat ISO = "ALL" separately - return stored polygon OR define big bounding box.
-  if ("all" %in% tolower(ISO)) {
-    if (exists("all_polygons", envir = .malariaAtlasHidden)) {
-      Shp_polygon <- .malariaAtlasHidden$all_polygons
-      return(Shp_polygon)
-    } else {
-      extent <- matrix(c(-180, -60, 180, 85), nrow = 2)
-    }
-  }
-
-  ##if not using extent - check to see if we have a previosuly stored version of the shapefile we can use.
-  if (is.null(extent)) {
-    if (exists("stored_polygons", envir = .malariaAtlasHidden)) {
-      if (all(
-        unlist(lapply(
-          X = country_input,
-          FUN = function(x)
-            paste(x, admin_num, sep = "_")
-        )) %in% unique(.malariaAtlasHidden$stored_polygons$country_level)
-      )) {
-        Shp_polygon <-
-          .malariaAtlasHidden$stored_polygons[.malariaAtlasHidden$stored_polygons$country_level %in% unlist(lapply(
-            X = country_input,
-            FUN = function(x)
-              paste(x, admin_num, sep = "_")
-          )), ]
-
-        return(Shp_polygon)
-      }
-    }
-  }
-
-  # if extent is specified, use this for geoserver query URL
-  if (!is.null(extent)) {
-    URL_filter <-
-      paste("&bbox=", paste(c(extent[2, 1], extent[1, 1], extent[2, 2], extent[1, 2]), collapse = ","), sep = "")
-    #otherwise by default use object country names to download shapefiles for these countries only via cql_filter.
-  } else {
-    URL_filter <-
-      paste(
-        "&cql_filter=iso%20IN%20(",
-        paste(
-          "%27",
-          country_input,
-          "%27",
-          collapse = ",",
-          sep = ""
-        ),
-        ")",
-        sep = ""
-      )
-  }
-
-  #define which admin levels are queried and return as a list full geoserver query URL
-  base_URL <-
-    "https://malariaatlas.org/geoserver/ows?service=wfs&version=2.0.0&request=GetFeature&outputFormat=shape-zip&srsName=EPSG:4326"
->>>>>>> e790317eafd0ae3164ff98a3d5dc73cdd48c2de5
-  
   features_list <-
     future.apply::future_lapply(admin_level, function(individual_admin_level) {
       dataset_id_admin_level <-
@@ -244,82 +153,5 @@ getShp <- function(country = NULL,
   
   return(invisible(df_features))
 
-
-
 }
 
-<<<<<<< HEAD
-=======
-  Shp_polygon <- lapply(URL_input, downloadShp)
-  if (length(admin_level) == 1 & !("all" %in% admin_level)){
-    Shp_polygon <- Shp_polygon[[paste(admin_level)]]
-  } else {
-    Shp_polygon <- do.call(what = rbind, args = Shp_polygon)
-  }
-
-  Shp_polygon$country_level <-
-    paste(Shp_polygon$iso, "_", Shp_polygon$admn_level, sep = "")
-  
-
-  if ("all" %in% tolower(ISO)) {
-    .malariaAtlasHidden$all_polygons <- Shp_polygon
-  } else if (is.null(extent)) {
-    if (!exists("stored_polygons", envir = .malariaAtlasHidden)) {
-      .malariaAtlasHidden$stored_polygons <- Shp_polygon
-    } else if (exists("stored_polygons", envir = .malariaAtlasHidden)) {
-      new_shps <-
-        Shp_polygon[!Shp_polygon$country_level %in% unique(.malariaAtlasHidden$stored_polygons$country_level), ]
-      .malariaAtlasHidden$stored_polygons <-
-        rbind(.malariaAtlasHidden$stored_polygons, new_shps)
-    }
-  }
-  
-  return(Shp_polygon)
-}
-
-#Define a few utility funcitons:
-
-#define function that downloads a shapefiles from map geoserver to tempdir and loads this into R
-downloadShp <- function(URL) {
-  # create a temporary filepath & directory to download shapefiles from MAP geoserver
-  td <- tempdir()
-  shpdir <- file.path(td, "shp")
-  dir.create(shpdir, showWarnings = FALSE)
-  temp <- tempfile(pattern = "shp", tmpdir = shpdir, fileext = ".zip")
-  uzipdir <- file.path(shpdir, sub("^([^.]*).*", "\\1", basename(temp)))
-  dir.create(uzipdir)
-  # download shapefile to temp directory & extract shapefilepath & layername
-  
-  r <- try(httr::GET(utils::URLencode(URL), httr::write_disk(temp, overwrite = TRUE)))
-  if(inherits(r, 'try-error')){
-    message(r[1])
-    return(r)
-  }
-  
-  utils::unzip(temp, exdir = uzipdir)
-  shp <- dir(uzipdir, "*.shp$")
-  shp.path <- file.path(uzipdir, shp)
-  lyr <- sub(".shp$", "", shp)
-
-  ## read shapefile into R
-  shapefile_dl <- sf::st_read(dsn = shp.path, layer = lyr)
-  
-  extra_cols <- c("id_0", "name_1", "id_1", "code_1", "type_1", "name_2", "id_2", "code_2", "type_2", "name_3", "id_3", "code_3", "type_3")
-  for (col_name in setdiff(extra_cols, names(shapefile_dl))) {
-    shapefile_dl[[col_name]] <- col_name
-  }
-  
-  shapefile_dl <-  shapefile_dl[,c("iso","admn_level",
-                                 "name_0","id_0","type_0","name_1",
-                                 "id_1","type_1","name_2","id_2",
-                                 "type_2","name_3","id_3",
-                                 "type_3","source")]
-  
-  ## delete temporary directory and return shapefile object
-  on.exit(unlink(shpdir, recursive = TRUE))
-  return(shapefile_dl)
-}
-
-
-
->>>>>>> e790317eafd0ae3164ff98a3d5dc73cdd48c2de5
