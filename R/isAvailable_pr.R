7bf0f22adcc543a2368435b4adb8b0a8c2fde2e6
sarahmconnor <sarah.connor@telethonkids.org.au>
2023-09-26 17:24:52 +0800

    Updated isAvailable_pr to use version instead of dataset_id, also deprecated meaningless input parameter sourcedata, and added ... to isAvailable so that version can be passed in there and then passed onto isAvailable_pr or isAvailable_vec

--- R/isAvailable_pr.R
+++ R/isAvailable_pr.R
@@ -4,12 +4,13 @@
 #'
 #' @return \code{isAvailable_pr} returns a named list of input locations with information regarding data availability.
 #'
-#' @param sourcedata by default this is "pr points", highlighting the dataset to be searched
+#' @param sourcedata deprecated argument. Please remove it from your code.
 #' @param country string containing name of desired country, e.g. \code{ c("Country1", "Country2", ...)} (use one of \code{country} OR \code{ISO} OR \code{continent}, not combined)
 #' @param ISO string containing ISO3 code for desired country, e.g. \code{c("XXX", "YYY", ...)} (use one of \code{country} OR \code{ISO} OR \code{continent}, not combined)
 #' @param continent  string containing name of continent for desired data, e.g. \code{c("Continent1", "Continent2", ...)}(use one of \code{country} OR \code{ISO} OR \code{continent}, not combined)
 #' @param full_results By default this is FALSE meaning the function only gives a message outlining whether specified country is available, if \code{full_results == TRUE}, the function returns a named list outlining data availability.
-#' @param dataset_id A character string specifying the dataset ID. Is NULL by default, and the most recent version of the pr points or vector points will be selected. #'
+#' @param version (optional) The PR dataset version to use If not provided, will just use the most recent version of PR data. (To see available version options, 
+#' use listPRPointVersions)
 #'
 #' @return if \code{full_results == TRUE}, a named list is returned with the following elements:
 #' \enumerate{
@@ -26,13 +27,17 @@
 #' }
 #' @export isAvailable_pr
 
-isAvailable_pr <- function(sourcedata = "pr points", country = NULL, ISO = NULL, continent = NULL, full_results = FALSE, dataset_id = NULL) {
+isAvailable_pr <- function(sourcedata = NULL, country = NULL, ISO = NULL, continent = NULL, full_results = FALSE, version = NULL) {
+  
+  if (!is.null(sourcedata)) {
+    lifecycle::deprecate_warn("1.5.0", "isAvailable_pr(sourcedata)", details = "The argument 'sourcedata' has been deprecated. It will be removed in the next version. It has no meaning.")
+  }
 
   if(is.null(country) & is.null(ISO) & is.null(continent)){
     stop('Must specify one of country, ISO or continent.')
   }
   
-  available_countries_pr <- listPoints(printed = FALSE, sourcedata = "pr points", dataset_id = dataset_id)
+  available_countries_pr <- listPoints(printed = FALSE, sourcedata = "pr points", version = version)
   
   capwords <- function(string) {
     cap <- function(s) {
